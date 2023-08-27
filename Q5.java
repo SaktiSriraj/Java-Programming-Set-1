2afc28ac71fb72216c9dbd599f504a3695686d56
Sakti Sriraj Mishra <saktisriraj.0214@gmail.com>
2023-08-28 01:54:00 +0530

    Update Q5.java

---
+++ Q5.java
@@ -0,0 +1,20 @@
+/*
+ Define a function to create a lookup table of size 216 whose value is the parity bits of the index.
+ */
+
+public class Q5 {
+	public static int computePartity(int x){
+        int result = 0;
+		while (x != 0) {
+		    result^= 1;
+		    x &= (x - 1);
+		}
+		return result;
+	}
+	public static void lookupTable(){
+	        int parity[] =new int[(int)Math.pow(2, 16)];
+	        for(int i=0;i<parity.length;i++){
+	            parity[i]=computePartity(i);
+	        }
+    	} 
+}
