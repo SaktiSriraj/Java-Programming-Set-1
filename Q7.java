62447fc5608e833dd8c0a2844eb16c4e5fff4577
Sakti Sriraj Mishra <saktisriraj.0214@gmail.com>
2023-08-28 01:55:56 +0530

    Update Q7.java

--- Q7.java
+++ Q7.java
@@ -3,7 +3,7 @@
  */
 
 import java.util.Scanner;
-public class A1Q7 {
+public class Q7 {
 	public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
