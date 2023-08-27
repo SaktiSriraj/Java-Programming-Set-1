6cf49c60d957345d7f0ab8c354c80f53a5d7c302
Sakti Sriraj Mishra <saktisriraj.0214@gmail.com>
2023-08-28 01:42:55 +0530

    Update and rename A1Q1.java to S1Q01.java

--- S1Q01.java
+++ S1Q01.java
@@ -1,6 +1,6 @@
 /*Write a java program to count the number of bits that are set 1 in an integer. Also prove that time complexity is O(n) where n is the number of bits*/
 
-public class A1Q1 {
+public class S1Q01 {
 	public static void main(String[] args) {
         int n=10;
 		int c=0;
